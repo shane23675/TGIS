86d46cc0c86d8f29b31178ab5e9edf270f604d63
AKA迪迪弄 <wl00494849@gmail.com>
2019-11-07 21:21:26 +0800

    簡單的管理員登入系統

---
+++ TGIS/Controllers/LoginForAdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TGIS.Models;
+
+namespace TGIS.Controllers
+{
+    
+    public class LoginForAdminController : Controller
+    {
+        TGISDBEntities db = new TGISDBEntities();
+        // GET: Login
+        public ActionResult LoginForAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginForAdmin(string ac, string pwd)
+        {
+            var user = db.Administrators.Where(m => m.Account == ac).Where(m=>m.Password==pwd).FirstOrDefault();
+            if (user !=null)
+            {
+                ViewBag.user = user;
+                return RedirectToAction("ShopIndex", "Shop");
+            }
+            ViewBag.Error = "帳號密碼錯誤";
+            return View();
+        }
+    }
+}
