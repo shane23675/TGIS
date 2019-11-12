8851e60ece0a1c9104c812677947fa68644035c0
彥廷 彥廷 <LF2857390@gmail.com>
2019-11-12 15:27:15 +0800

    metadata修改

--- TGIS/Models/Metadata.cs
+++ TGIS/Models/Metadata.cs
@@ -14,11 +14,9 @@ namespace TGIS.Models
     {
         [DisplayName("管理員編號")]
         public string ID { get; set; }
-        [DisplayName("管理員帳號")]
-        [Required(ErrorMessage = "請輸入帳號")]
+        [DisplayName("管理員帳號"),Required(ErrorMessage = "請輸入帳號")]
         public string Account { get; set; }
-        [DisplayName("管理員密碼")]
-        [Required(ErrorMessage = "請輸入密碼")]
+        [DisplayName("管理員密碼"), Required(ErrorMessage = "請輸入密碼")]
         public string Password { get; set; }
     }
     //優惠卷主檔
@@ -26,23 +24,23 @@ namespace TGIS.Models
     public partial class Coupon { }
     public class MetadataCoupon
     {
-        [DisplayName("活動編號")]
+        [DisplayName("活動編號"),StringLength(11)]
         public string ID { get; set; }
-        [DisplayName("店家會員編號")]
+        [DisplayName("店家會員編號"),StringLength(6)]
         public string ShopID { get; set; }
-        [DisplayName("優惠內容")]
+        [DisplayName("優惠內容"),StringLength(100)]
         public string Content { get; set; }
-        [DisplayName("開始使用日期")]
+        [DisplayName("開始使用日期"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime BeginDate { get; set; }
-        [DisplayName("到期日期")]
+        [DisplayName("到期日期"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime ExpireDate { get; set; }
-        [DisplayName("需求點數")]
+        [DisplayName("需求點數"),Range(0,int.MaxValue)]
         public int PointsRequired { get; set; }
         [DisplayName("可換張數"), Range(0, int.MaxValue)]
         public Nullable<int> LimitedAmount { get; set; }
         [DisplayName("剩餘張數"), Range(0, int.MaxValue)]
         public Nullable<int> RemainedAmount { get; set; }
-        [DisplayName("總兌換人數")]
+        [DisplayName("總兌換人數"), Range(0, int.MaxValue)]
         public Nullable<int> ExchangedAmount { get; set; }
         [DisplayName("活動狀態")]
         public bool IsAvailable { get; set; }
@@ -54,7 +52,7 @@ namespace TGIS.Models
     {
         [DisplayName("縣市編號")]
         public int ID { get; set; }
-        [DisplayName("縣市名稱")]
+        [DisplayName("縣市名稱"),StringLength(10)]
         public string CityName { get; set; }
     }
     //公告列表
@@ -64,11 +62,11 @@ namespace TGIS.Models
     {
         [DisplayName("公告編號")]
         public string ID { get; set; }
-        [DisplayName("發佈管理員")]
+        [DisplayName("發佈管理員"),StringLength(4)]
         public string AdministratorID { get; set; }
-        [DisplayName("發佈日期")]
+        [DisplayName("發佈日期"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime AnnouncedDate { get; set; }
-        [DisplayName("標題")]
+        [DisplayName("標題"),StringLength(40)]
         public string Title { get; set; }
         [DisplayName("內文")]
         public string Content { get; set; }
@@ -80,11 +78,11 @@ namespace TGIS.Models
     {
         [DisplayName("回報編號")]
         public string ID { get; set; }
-        [DisplayName("來信時間")]
+        [DisplayName("來信時間"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime ReceivedDate { get; set; }
-        [DisplayName("檢舉人")]
+        [DisplayName("檢舉人"),StringLength(6)]
         public string Plaintiff { get; set; }
-        [DisplayName("被檢舉人")]
+        [DisplayName("被檢舉人"),StringLength(6)]
         public string Defendent { get; set; }
         [DisplayName("檢舉內容")]
         public string Content { get; set; }
@@ -100,7 +98,7 @@ namespace TGIS.Models
         public int ID { get; set; }
         [DisplayName("縣市邊號")]
         public int CityID { get; set; }
-        [DisplayName("行政區名稱")]
+        [DisplayName("行政區名稱"),StringLength(10)]
         public string DistrictName { get; set; }
     }
     //訊息列表
@@ -112,11 +110,11 @@ namespace TGIS.Models
         public int ID { get; set; }
         [DisplayName("揪桌編號")]
         public string TeamID { get; set; }
-        [DisplayName("留言時間")]
+        [DisplayName("留言時間"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime MessageDate { get; set; }
-        [DisplayName("留言者")]
+        [DisplayName("留言者"),StringLength(6)]
         public string Speaker { get; set; }
-        [DisplayName("留言內容")]
+        [DisplayName("留言內容"),StringLength(300)]
         public string Content { get; set; }
         [DisplayName("是否為私人訊息")]
         public bool IsPrivate { get; set; }
@@ -132,13 +130,13 @@ namespace TGIS.Models
         public string ShopID { get; set; }
         [DisplayName("宣傳圖")]
         public byte[] ADImage { get; set; }
-        [DisplayName("標題")]
+        [DisplayName("標題"),StringLength(20)]
         public string Title { get; set; }
-        [DisplayName("內文")]
+        [DisplayName("內文"),StringLength(300)]
         public string Content { get; set; }
-        [DisplayName("開啟日期")]
+        [DisplayName("開啟日期"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime BeginDate { get; set; }
-        [DisplayName("關閉日期")]
+        [DisplayName("關閉日期"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime EndDate { get; set; }
         [DisplayName("被點閱數")]
         public int Clicks { get; set; }
@@ -162,23 +160,23 @@ namespace TGIS.Models
     {
         [DisplayName("會員編號")]
         public string ID { get; set; }
-        [DisplayName("會員帳號")]
+        [DisplayName("會員帳號"),StringLength(20)]
         public string Account { get; set; }
-        [DisplayName("會員密碼")]
+        [DisplayName("會員密碼"),StringLength(20)]
         public string Password { get; set; }
-        [DisplayName("暱稱")]
+        [DisplayName("暱稱"),StringLength(10)]
         public string NickName { get; set; }
         [DisplayName("照片")]
         public byte[] PlayerImage { get; set; }
-        [DisplayName("信箱")]
+        [DisplayName("信箱"),]
         public string Email { get; set; }
         [DisplayName("性別")]
         public bool Gender { get; set; }
         [DisplayName("行政區編號")]
         public int DistrictID { get; set; }
-        [DisplayName("生日")]
+        [DisplayName("生日"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime Birthday { get; set; }
-        [DisplayName("持有點數量")]
+        [DisplayName("持有點數量"),Range(0,int.MaxValue)]
         public int Points { get; set; }
         [DisplayName("是否停權")]
         public bool IsBanned { get; set; }
@@ -195,7 +193,7 @@ namespace TGIS.Models
         public string CouponID { get; set; }
         [DisplayName("優惠卷編碼")]
         public long Code { get; set; }
-        [DisplayName("兌換時間")]
+        [DisplayName("兌換時間"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime ExchangedDate { get; set; }
         [DisplayName("是否使用")]
         public bool IsUsed { get; set; }
@@ -209,11 +207,11 @@ namespace TGIS.Models
         public int ID { get; set; }
         [DisplayName("玩家會員編號")]
         public string PlayerID { get; set; }
-        [DisplayName("點數變化")]
+        [DisplayName("點數變化"),Range(0,int.MaxValue)]
         public int ChangedAmount { get; set; }
-        [DisplayName("變動原因")]
+        [DisplayName("變動原因"),StringLength(20)]
         public string Cause { get; set; }
-        [DisplayName("獲得時間")]
+        [DisplayName("獲得時間"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime ChangedDate { get; set; }
     }
     //相關教學連結
@@ -223,9 +221,9 @@ namespace TGIS.Models
     {
         [DisplayName("流水號")]
         public int ID { get; set; }
-        [DisplayName("網址")]
+        [DisplayName("網址"),StringLength(1000)]
         public string Url { get; set; }
-        [DisplayName("桌遊編號")]
+        [DisplayName("桌遊編號"),StringLength(6)]
         public string TableGameID { get; set; }
     }
     //店家主檔
@@ -235,17 +233,17 @@ namespace TGIS.Models
     {
         [DisplayName("會員編號")]
         public string ID { get; set; }
-        [DisplayName("帳號")]
+        [DisplayName("帳號"),StringLength(20)]
         public string Account { get; set; }
-        [DisplayName("密碼")]
+        [DisplayName("密碼"),StringLength(20)]
         public string Password { get; set; }
-        [DisplayName("店家名稱")]
+        [DisplayName("店家名稱"),StringLength(40)]
         public string ShopName { get; set; }
-        [DisplayName("營業時間")]
+        [DisplayName("營業時間"),StringLength(100)]
         public string OpeningHours { get; set; }
         [DisplayName("行政區編號")]
         public int DistrictID { get; set; }
-        [DisplayName("地址")]
+        [DisplayName("地址"),StringLength(60)]
         public string Address { get; set; }
         [DisplayName("有無低消")]
         public bool IsMinConsumeRequired { get; set; }
@@ -253,23 +251,23 @@ namespace TGIS.Models
         public bool IsFoodAcceptable { get; set; }
         [DisplayName("場地規模")]
         public string AreaScale { get; set; }
-        [DisplayName("電話")]
+        [DisplayName("電話"),StringLength(25)]
         public string Tel { get; set; }
         [DisplayName("信箱")]
         public string Email { get; set; }
-        [DisplayName("Line")]
+        [DisplayName("Line"),StringLength(30)]
         public string Line { get; set; }
         [DisplayName("官網")]
         public string Website { get; set; }
         [DisplayName("是否啟用VIP")]
         public bool IsVIP { get; set; }
-        [DisplayName("到期日")]
+        [DisplayName("到期日"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> ExpireDate { get; set; }
         [DisplayName("累積加值時數")]
         public Nullable<int> AccumulatedHours { get; set; }
         [DisplayName("帳號是否啟用")]
         public bool IsAccountEnabled { get; set; }
-        [DisplayName("內容描述")]
+        [DisplayName("內容描述"),StringLength(1000)]
         public string Description { get; set; }
     }
     //桌遊主檔
@@ -279,27 +277,19 @@ namespace TGIS.Models
     {
         [DisplayName("桌遊編號")]
         public string ID { get; set; }
-        [DisplayName("中文名稱")]
-        [Required(ErrorMessage = "請輸入中文名稱")]
-        [StringLength(40)]
+        [DisplayName("中文名稱"),Required(ErrorMessage = "請輸入中文名稱"),StringLength(40)]
         public string ChineseName { get; set; }
-        [Required(ErrorMessage = "請輸入英文名稱")]
-        [StringLength(100)]
-        [DisplayName("英文名稱")]
+        [DisplayName("英文名稱"),Required(ErrorMessage = "請輸入英文名稱"),StringLength(100)]
         public string EnglishName { get; set; }
         [DisplayName("是否為擴充版")]
         public bool IsExtended { get; set; }
-        [DisplayName("最小遊玩人數")]
-        [Range(1,10)]
+        [DisplayName("最小遊玩人數"),Range(1,10)]
         public short MinPlayer { get; set; }
-        [DisplayName("最大遊玩人數")]
-        [Range(1,30)]
+        [DisplayName("最大遊玩人數"),Range(1,30)]
         public short MaxPlayer { get; set; }
-        [DisplayName("平均遊戲時間")]
-        [Range(0, 240)]
+        [DisplayName("平均遊戲時間"),Range(0, 240)]
         public short AverageGamePeroid { get; set; }
-        [DisplayName("詳細遊戲時間")]
-        [StringLength(10)]
+        [DisplayName("詳細遊戲時間"),StringLength(10)]
         public string DetailGamePeroid { get; set; }
         [DisplayName("桌遊特色")]
         public string Feature { get; set; }
@@ -307,9 +297,9 @@ namespace TGIS.Models
         public string Rules { get; set; }
         [DisplayName("上架狀態")]
         public bool IsShown { get; set; }
-        [DisplayName("遊戲難度標籤")]
+        [DisplayName("遊戲難度標籤"),StringLength(3)]
         public string DifficultyTagID { get; set; }
-        [DisplayName("桌遊品牌標籤")]
+        [DisplayName("桌遊品牌標籤"),StringLength(3)]
         public string BrandTagID { get; set; }
     }
     //桌遊評論列表
@@ -319,11 +309,9 @@ namespace TGIS.Models
     {
         [DisplayName("流水號")]
         public int ID { get; set; }
-        [DisplayName("評論日期")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
+        [DisplayName("評論日期"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime CommentDate { get; set; }
-        [DisplayName("評論內容")]
-        [StringLength(500)]
+        [DisplayName("評論內容"),StringLength(500)]
         public string Content { get; set; }
         [DisplayName("是否隱藏")]
         public bool IsHidden { get; set; }
@@ -337,9 +325,9 @@ namespace TGIS.Models
     public partial class TableGameInShopDetail { }
     public class MetadataTableGameInShopDetail
     {
-        [DisplayName("桌遊編號")]
+        [DisplayName("桌遊編號"),StringLength(6)]
         public string TableGameID { get; set; }
-        [DisplayName("店家會員編號")]
+        [DisplayName("店家會員編號"),StringLength(6)]
         public string ShopID { get; set; }
         [DisplayName("是否販售")]
         public bool IsSale { get; set; }
@@ -352,12 +340,11 @@ namespace TGIS.Models
     public partial class TableGameVisitedStatistic { }
     public class MetadataTableGameVisitedStatistic
     {
-        [DisplayName("被閱覽時間")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
+        [DisplayName("被閱覽時間"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime VisitedDate { get; set; }
         [DisplayName("閱覽次數")]
         public int Clicks { get; set; }
-        [DisplayName("桌遊編號")]
+        [DisplayName("桌遊編號"),StringLength(6)]
         public string TableGameID { get; set; }
 
     }
@@ -368,7 +355,7 @@ namespace TGIS.Models
     {
         [DisplayName("標籤編號")]
         public string ID { get; set; }
-        [DisplayName("標籤名稱")]
+        [DisplayName("標籤名稱"),StringLength(15)]
         public string TagName { get; set; }
     }
     //揪桌主檔
@@ -378,27 +365,27 @@ namespace TGIS.Models
     {
         [DisplayName("揪桌編號")]
         public string ID { get; set; }
-        [DisplayName("店家會員編號")]
+        [DisplayName("店家會員編號"),StringLength(6)]
         public string ShopID { get; set; }
-        [DisplayName("主揪玩家編號")]
+        [DisplayName("主揪玩家編號"),StringLength(6)]
         public string LeaderPlayerID { get; set; }
-        [DisplayName("揪桌標題")]
+        [DisplayName("揪桌標題"),StringLength(10)]
         public string Title { get; set; }
-        [DisplayName("報名開始時間")]
+        [DisplayName("報名開始時間"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime ParticipateBeginDate { get; set; }
-        [DisplayName("報名結束時間")]
+        [DisplayName("報名結束時間"),DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime ParticipateEndDate { get; set; }
-        [DisplayName("遊玩開始時間")]
+        [DisplayName("遊玩開始時間"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime PlayBeginTime { get; set; }
-        [DisplayName("遊玩結束時間")]
+        [DisplayName("遊玩結束時間"), DisplayFormat(DataFormatString = "{0:yyyy/MM/DD}", ApplyFormatInEditMode = true)]
         public System.DateTime PlayEndTime { get; set; }
-        [DisplayName("成團人數")]
+        [DisplayName("成團人數"),StringLength(20)]
         public string MinPlayers { get; set; }
-        [DisplayName("遊玩形式")]
+        [DisplayName("遊玩形式"),StringLength(20)]
         public string PlayPreference { get; set; }
         [DisplayName("預估花費")]
         public Nullable<decimal> EstimatedCost { get; set; }
-        [DisplayName("其他資訊備註")]
+        [DisplayName("其他資訊備註"),StringLength(20)]
         public string Notes { get; set; }
         [DisplayName("團務狀態")]
         public string Status { get; set; }
