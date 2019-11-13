f525c6dc1610b62d4286f2838ea08d2d7e9eede7
alpha9527 <wl00494849@gmail.com>
2019-11-13 15:01:35 +0800

    衝突

--- TGIS/Controllers/LoginController.cs
+++ TGIS/Controllers/LoginController.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace TGIS.Controllers
 {
     public class LoginController : Controller
     {
+        SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TGISDBEntities"].ConnectionString);
         // GET: Login
-        public ActionResult Index()
+        public ActionResult LoginForPlayer(string acc,string pwd)
         {
-            return View();
+            string sql = "select ID from Player where Account=@acc and Password=@pwd";
+            SqlCommand cmd = new SqlCommand(sql,Conn);
+            cmd.Parameters.AddWithValue("Account", acc);
+            cmd.Parameters.AddWithValue("Password", pwd);
+            SqlDataReader rd;
+
+            Conn.Open();
+            rd = cmd.ExecuteReader();
+            if (rd.Read())
+            {
+                Conn.Close();
+                return RedirectToAction("PlayerIndex","Player");
+            }
+            Conn.Close();
+            ViewBag.Error = "帳號密碼錯誤";
+            return View(acc);
         }
     }
 }
--- TGIS/Controllers/ajaxController.cs
+++ TGIS/Controllers/ajaxController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -31,15 +33,34 @@ namespace TGIS.Controllers
             }
             return Content(sb.ToString());
         }
-        public  ActionResult AccountRepeat(string account)
+        public  ActionResult AccountRepeat(string account, string tableName)
         {
-            var ac = db.Shops.Where(m => m.Account == account).FirstOrDefault();
-            if (ac == null)
+            bool isRepeated = false;
+            switch (tableName)
+            {
+                case "Shops":
+                    isRepeated = RepeatCheck(db.Shops, account);
+                    break;
+                case "Players":
+                    isRepeated = RepeatCheck(db.Players, account);
+                    break;
+            }
+            if (!isRepeated)
             {
                 return Content("帳號可使用".ToString());
             }
             return Content("帳號重複".ToString());
         }
-
+        bool RepeatCheck<T>(IEnumerable<T> table, string account)
+        {
+            PropertyInfo info = typeof(T).GetProperty("Account");
+            foreach (T item in table)
+            {
+                string acc = info.GetValue(item).ToString();
+                if (acc == account)
+                    return true;
+            }
+            return false;
+        }
     }
 }
