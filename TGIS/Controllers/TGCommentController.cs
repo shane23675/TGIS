37fe5cf69a98b205f4ff774c8afde2cb3b0fb403
AKA迪迪弄 <wl00494849@gmail.com>
2019-11-17 16:39:22 +0800

    一堆功能跟大改 登入系統新增Session判斷 玩家忘記密碼功能(完成) 玩家評論桌遊功能(完成)

---
+++ TGIS/Controllers/TGCommentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TGIS.Models;
+
+
+namespace TGIS.Controllers
+{
+    public class TGCommentController : Controller
+    {
+        TGISDBEntities db = new TGISDBEntities();
+        // GET: TGComment
+        public ActionResult CreateTGComment(string tId,string comment)
+        {
+            var pId =Session["PlayerID"].ToString();
+            if (ModelState.IsValid)
+            {
+                TableGameComment tgc = new TableGameComment();
+                tgc.PlayerID = pId;
+                tgc.TableGameID = tId;
+                tgc.CommentDate = DateTime.Now;
+                tgc.Content = comment;
+                tgc.IsHidden = false;
+                db.TableGameComments.Add(tgc);
+                db.SaveChanges();
+
+                return RedirectToAction("ShowTableGameDetail","TableGame",new { tableGameID = tId });
+            }
+            return View();
+        }
+    }
+}
