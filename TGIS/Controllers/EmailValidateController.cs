889fb04fbff6929c19f7dd11a81d163c65b9febd
AKA迪迪弄 <wl00494849@gmail.com>
2019-11-18 22:35:51 +0800

    信箱驗證(完成)

--- TGIS/Controllers/EmailValidateController.cs
+++ TGIS/Controllers/EmailValidateController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.SqlClient;
-using System.Configuration;
+using TGIS.Models;
 
 namespace TGIS.Controllers
 {
     public class EmailValidateController : Controller
     {
-        SqlConnection Conn = new SqlConnection(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\TGISDB.mdf;integrated security=True;connect timeout=30;MultipleActiveResultSets=True;App=EntityFramework&quot;");
+        TGISDBEntities db = new TGISDBEntities();
         // GET: Default
-        public ActionResult Index()
+        public ActionResult EmailValidate(string Email,string id)
         {
+            var content = $"http://localhost:55525/EmailValidate/EmailRepeat?id={id}";
+            AutoEmail.AutoEmailSend(Email, "有桌方遊:信箱驗證",content);
+            return View();
+        }
+        public ActionResult EmailRepeat(string id)
+        {
+            Player player = db.Players.Where(m => m.ID == id).FirstOrDefault();
+            player.IsEmailValid = true;
+            db.SaveChanges();
             return View();
         }
     }
