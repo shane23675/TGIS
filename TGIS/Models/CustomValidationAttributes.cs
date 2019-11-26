57b36a7e75548565192fcd8b853884397ebebe39
紹恩 黃 <kevin0976184751@gmail.com>
2019-11-26 15:43:35 +0800

    各式介面優化

--- TGIS/Models/CustomValidationAttributes.cs
+++ TGIS/Models/CustomValidationAttributes.cs
@@ -8,4 +8,18 @@ using TGIS.Models;
 //請將自定義的驗證Attribute寫在這裡並標註功能
 namespace TGIS.Models
 {
+    //驗證生日是否在今天以前
+    public class BirthdayValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                ErrorMessage = "生日為必填欄位";
+                return false;
+            }
+            ErrorMessage = "生日日期不合法";
+            return (DateTime)value < DateTime.Now;
+        }
+    }
 }
