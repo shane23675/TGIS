6e5671d27b1b51f191283362c9f16e85ff9993f7
iris1314521 <iris1314521@gmail.com>
2019-12-10 16:18:23 +0800

    check

--- TGIS/Controllers/ShopController.cs
+++ TGIS/Controllers/ShopController.cs
@@ -281,5 +281,12 @@ namespace TGIS.Controllers
                 return Content("true");
             return Content("false");
         }
+
+        //查看店家活動列表
+        public ActionResult MgShopOfferList(string ShopID)
+        {
+            return View(db.NormalOffers.Where(m=>m.ShopID==ShopID).ToList());
+        }
+
     }
 }
