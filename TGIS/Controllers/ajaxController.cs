5de3d2e9d68087aa9c4a052e4a954cc40b19d288
iris1314521 <iris1314521@gmail.com>
2019-12-10 14:02:09 +0800

    帳號重複確認

--- TGIS/Controllers/ajaxController.cs
+++ TGIS/Controllers/ajaxController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,35 +33,6 @@ namespace TGIS.Controllers
                 }
             }
             return Content(sb.ToString());
-        }
-        public  ActionResult AccountRepeat(string val, string tableName,string property)
-        {
-            bool RepeatCheck<T>(IEnumerable<T> table, string v, string p)
-            {
-                PropertyInfo info = typeof(T).GetProperty(p);
-                foreach (T item in table)
-                {
-                    string value = info.GetValue(item).ToString();
-                    if (value == v)
-                        return true;
-                }
-                return false;
-            }
-            bool isRepeated = false;
-            switch (tableName)
-            {
-                case "Shops":
-                    isRepeated = RepeatCheck(db.Shops, val, property);
-                    break;
-                case "Players":
-                    isRepeated = RepeatCheck(db.Players, val, property);
-                    break;
-            }
-            if (!isRepeated)
-            {
-                return Content("可使用".ToString());
-            }
-            return Content("已使用過".ToString());
         }
         [HttpPost]
         public ActionResult PasswordChange(string oldpwd, string newpwd, string pwdrepeat,string userId)
