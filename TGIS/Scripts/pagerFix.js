120becc27f8a7601888afbdc2165c3594ba16bd6
紹恩 黃 <kevin0976184751@gmail.com>
2019-12-13 11:39:26 +0800

    店家列表分頁功能完成80%

---
+++ TGIS/Scripts/pagerFix.js
@@ -0,0 +1,20 @@
+﻿//修正店家及桌遊篩選時無法使用分頁的問題
+
+//找到所有的分頁用連結<a>
+var pageLinks = $(".pagination  a");
+for (var i = 0; i < pageLinks.length; i++) {
+    //找到該連結的href的最後一個字即為頁碼
+    let link = pageLinks.eq(i);
+    let href = link.attr("href");
+    if (href == undefined)
+        continue;
+    //刪除該連結的原本效果
+    link.attr("href", "javascript:;");
+    let page = parseInt(href[href.length - 1]);
+    link.click(function () {
+        //將<input id="page">的value改為該頁碼
+        $("#page").val(page);
+        //送出表單Post請求以達到篩選效果
+        $("#filter").submit();
+    })
+}
