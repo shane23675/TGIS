54c4d0fa4ed17a1315ec4f6c1e1299db1bc9e7f3
Helosole <kevin0976184751@gmai.com>
2019-12-19 00:29:10 +0800

    新增揪桌資料產生器

--- TGIS/Controllers/ToolController.cs
+++ TGIS/Controllers/ToolController.cs
@@ -25,13 +25,59 @@ namespace TGIS.Controllers
         //修正RelevantLinks網址的方法
         public ActionResult RelevantLinksReplace()
         {
-            //var links = db.RelevantLinks.ToList();
-            //foreach (var link in links)
-            //{
-            //    link.Url = link.Url.Replace("watch?v=", "embed/");
-            //}
-            //db.SaveChanges();
+            var links = db.RelevantLinks.ToList();
+            foreach (var link in links)
+            {
+                link.Url = link.Url.Replace("watch?v=", "embed/");
+            }
+            db.SaveChanges();
             return RedirectToAction("Test");
+        }
+
+        //新增揪桌測試資料的方法
+        public string CreateTeamTestData(int n)
+        {
+            Random r = new Random();
+            var players = db.Players.ToList();
+            //製造出對應筆數的Team及其相關資料
+            for (int i = 0; i < n; i++)
+            {
+                //將玩家集合打亂
+                players = players.OrderBy(p => Guid.NewGuid()).ToList();
+                string teamID = UsefulTools.GetNextID(db.Teams, 1);
+                int playerAmount = r.Next(2, 6);
+                DateTime playDate = DateTime.Today.AddDays(r.Next(-90, -1)).Date;
+                Team team = new Team
+                {
+                    ID = teamID,
+                    ShopID = "S00014",
+                    Title = "測試用資料",
+                    MinPlayer = playerAmount,
+                    MaxPlayer = playerAmount,
+                    EstimatedCost = 0,
+                    IsCanceled = false,
+                    IsClosed = false,
+                    IsConfirmedByShop = true,
+                    IsRequestSent = true,
+                    LeaderPlayerID = players[0].ID,
+                    PlayDate = playDate,
+                    ParticipateEndDate = playDate.AddDays(-1),
+                    PlayBeginTime = new TimeSpan(8, 0, 0),
+                    PlayEndTime = new TimeSpan(10, 0, 0),
+                    Notes = "測試用"
+                };
+                db.Teams.Add(team);
+                db.SaveChanges();
+
+                //存入後立刻取出並填入其他玩家資料
+                Team hotNewTeam = db.Teams.Find(teamID);
+                for (int j = 1; j < playerAmount; j++)
+                {
+                    hotNewTeam.OtherPlayers.Add(players[j]);
+                }
+                db.SaveChanges();
+            }
+            return "Success";
         }
     }
 }
