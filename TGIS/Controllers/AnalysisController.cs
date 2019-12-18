143fef75b2a1bb7098886b027fd7895acea442f2
紹恩 黃 <kevin0976184751@gmail.com>
2019-12-18 16:51:05 +0800

    桌遊趨勢分析 資料傳遞方式修改

--- TGIS/Controllers/AnalysisController.cs
+++ TGIS/Controllers/AnalysisController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,13 +15,13 @@ namespace TGIS.Controllers
         //揪桌客群分析(Ajax)
         [HttpPost]
         [CenterLogin(CenterLogin.UserType.Shop)]
-        public ActionResult TeamStatistic(int moonsFromNow)
+        public ActionResult TeamStatistic(int monthsFromNow)
         {
             Shop s = db.Shops.Find(Session["ShopID"].ToString());
             //只取有被接受訂位的團
             var teams = s.Teams.Where(t => t.IsConfirmedByShop != null && (bool)t.IsConfirmedByShop).ToList();
             //篩選出距今指定月數的資料(0表示30天內，1表示31~60天...)
-            teams = teams.Where(t => DateTime.Now.Subtract(t.PlayDate).Days / 30 == moonsFromNow).ToList();
+            teams = teams.Where(t => DateTime.Now.Subtract(t.PlayDate).Days / 30 == monthsFromNow).ToList();
             /*
              *  將過去一個月的揪桌資料統計出來：
              *  需要性別、年齡層、行政區
@@ -62,39 +63,47 @@ namespace TGIS.Controllers
         //桌遊趨勢分析需要的類別
         class TGStatistic
         {
-            public int Month { get; set; }
-            public int Clicks { get; set; }
+            public string Name { get; set; }
+            public int[] Clicks { get; set; } = new int[12];
         }
-        //桌遊趨勢分析(Ajax)
+        //桌遊趨勢分析
+        public ActionResult TableGameTrend()
+        {
+            return View();
+        }
         [HttpPost]
-        [CenterLogin(CenterLogin.UserType.Shop)]
+        //[CenterLogin(CenterLogin.UserType.Shop)]
         public ActionResult TableGameTrend(string[] tableGameIDs)
         {
-            /*
-             *  店家選擇桌遊
-             *  取得這些桌遊前一年份的閱覽紀錄
-             *  回傳Json：月份、點擊量
-             */
-            //建立裝資料的容器
-            var data = new Dictionary<string, List<TGStatistic>>();
-
-            foreach(string id in tableGameIDs)
+            //取得一年內的月份資料(如果現在是6月，則輸出為[7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6] )
+            int nowMonth = DateTime.Now.Month;
+            int[] months = new int[12];
+            for (int i = 0; i < 12; i++)
             {
-                data[id] = new List<TGStatistic>();
-                //找到對應桌遊的統計資料
-                var statistics = db.TableGames.Find(id).TableGameVisitedStatistics.ToList();
-                //依據月份遞減找到對應的資料
+                int m = nowMonth - i;
+                months[11 - i] = m > 0 ? m : m + 12;
+            }
+
+            //取得桌遊統計資料(每筆資料形態皆如TGStatistic)
+            TGStatistic[] data = new TGStatistic[tableGameIDs.Length];
+            int c = 0;
+            foreach (string id in tableGameIDs)
+            {
+                var game = db.TableGames.Find(id);
+                //往資料集合添加一個TGStatistic物件
+                data[c] = new TGStatistic { Name = game.ChineseName, Clicks = new int[12] };
+                //依據月份順序往TGStatistic物件中的Clicks填入對應的點擊量
                 for (int i = 0; i < 12; i++)
                 {
-                    var statistic = statistics.Where(s => s.VisitedDate.ToString("yyyy/MM") == DateTime.Today.AddMonths(-i).ToString("yyyy/MM")).FirstOrDefault();
-                    int clicks = 0;
-                    if (statistic != null)
-                        clicks = statistic.Clicks;
-                    //將統計資料填入小容器
-                    data[id].Add(new TGStatistic{ Month = DateTime.Today.AddMonths(-i).Month, Clicks = clicks });
+                    var statistic = game.TableGameVisitedStatistics.Where(s => s.VisitedDate.ToString("yyyy/MM") == DateTime.Today.AddMonths(-i).ToString("yyyy/MM")).FirstOrDefault();
+                    data[c].Clicks[11 - i] = statistic == null ? 0 : statistic.Clicks;
                 }
+                c++;
             }
-            return Json(data);
+            //將資料傳入ViewBag
+            ViewBag.Months = JsonConvert.SerializeObject(months);
+            ViewBag.Data = JsonConvert.SerializeObject(data);
+            return View();
         }
 
         //優惠券行銷分析(Ajax)
